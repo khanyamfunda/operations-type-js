801379b9007f0231ef0f327e4d934a35c97b3ae8
khanyamfunda <khanyamfunda9@gmail.com>
2026-05-20 15:52:05 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,16 @@
+// Introduction to JavaScript Exercise 2
+
+const number1 = 33;
+const number2 = 11;
+const number1convstring = String(number1);
+
+const sum = number1 + number2;
+const product = number1 * number2;
+
+console.log("number1:", number2)
+console.log("number1:", number2)
+console.log("number1 as a string:", number1convstring)
+console.log("Type of number1:", typeof number1)
+console.log("Type of number1convstring;", typeof number1convstring)
+console.log("Sum(number1 + number2:", sum)
+console.log("Product(number1 * number2):", product)
